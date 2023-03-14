e7950761df828919629b3babc9a1205ed2560468
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-14 07:11:24 -0300

    feat: Create sketch of ExpensesApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
