bdd8c1d25028b6dbb052e78924009fe121952e66
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-14 07:13:49 -0300

    feat: Create and use MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,23 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
     );
   }
 }
