f43f4f630d595ae565ab2c33b2636ffb5421c509
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-14 08:05:01 -0300

    feat: Add R$ to value

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        transaction.value.toString(),
+                        'R\$ ${transaction.value}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
