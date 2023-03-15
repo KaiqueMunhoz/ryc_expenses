4b763795e4a9f06a45c965a25a725d0fade9ec90
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-15 07:48:13 -0300

    feat: Move TransactionForm code

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  const TransactionForm({Key key}) : super(key: key);
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  TransactionForm({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FlatButton(
+                  child: Text('Nova Transação'),
+                  textColor: Colors.purple,
+                  onPressed: () {
+                    print(titleController.text);
+                    print(valueController.text);
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,7 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -52,41 +51,7 @@ class MyHomePage extends StatelessWidget {
             elevation: 5,
           ),
           TransactionList(_transactions),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FlatButton(
-                        child: Text('Nova Transação'),
-                        textColor: Colors.purple,
-                        onPressed: () {
-                          print(titleController.text);
-                          print(valueController.text);
-                        },
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
+          
         ],
       ),
     );
