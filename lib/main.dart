81c47cda12c6ab2388d4685375f756f34d66c5c6
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-17 07:17:16 -0300

    feat: Add style of heading6 to theme

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,13 @@ class ExpensesApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
         fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
                 headline6: TextStyle(
