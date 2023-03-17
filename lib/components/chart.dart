96bf7b72b5bfe42172150fa65a0c29e64a112006
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-17 07:26:50 -0300

    feat: Create getter groupedTransaction

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class Chart extends StatelessWidget {
   const Chart({Key key}) : super(key: key);
 
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'value': 9.99};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
