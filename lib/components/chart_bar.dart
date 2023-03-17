674e8511b3f64ea4e47fc743287ff2c64e6b3931
Kaique Munhoz <munhoz13km@gmail.com>
2023-03-17 07:53:47 -0300

    feat: Add three parameters to ChartBar

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-  const ChartBar({Key key}) : super(key: key);
+  final String label;
+  final double value;
+  final double percentage;
+
+  const ChartBar({
+    Key key,
+    @required this.label,
+    @required this.value,
+    @required this.percentage,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
